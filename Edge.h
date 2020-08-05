3c03700ff0dfb07f0a424863c4d6c77428646d28
Alon Hacohen <ALONHACO@GMAIL.COM>
2020-08-05 03:11:25 +0300

    add classes

---
+++ Edge.h
@@ -0,0 +1,14 @@
+//
+// Created by User on 8/5/2020.
+//
+
+#ifndef MTM_FINAL_PROJECT_20_EDGE_H
+#define MTM_FINAL_PROJECT_20_EDGE_H
+
+
+class Edge {
+
+};
+
+
+#endif //MTM_FINAL_PROJECT_20_EDGE_H
---
+++ Graph.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 8/5/2020.
+//
+
+#ifndef MTM_FINAL_PROJECT_20_GRAPH_H
+#define MTM_FINAL_PROJECT_20_GRAPH_H
+
+#endif //MTM_FINAL_PROJECT_20_GRAPH_H
